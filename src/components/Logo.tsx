90b516ca64269c855d4eb5dfb60b2e266eae3afe
Luan Oak <99412966+luancarvalho7@users.noreply.github.com>
2025-09-18 00:19:09 -0300

    Update Logo URL

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,8 +4,8 @@ export default function Logo() {
   return (
     <div className="flex items-center justify-center">
       <img 
-        src="https://i.postimg.cc/vmrmrwmf/Prevent-Logo.jpg" 
-        alt="PREVENT!" 
+        src="https://n8n-minio.f35eaw.easypanel.host/closetiq/f18efc03-583a-4b96-8bb6-327f2dcac704" 
+        alt="Logo" 
         className="h-8 w-auto object-contain"
       />
     </div>
